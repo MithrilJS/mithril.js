a8d6f529d577098d3ab5f82f016578223f19f812
Eirik S. Morland <eirik@morland.no>
2014-03-23 08:42:15 +0100

    Make tests throw exception, add travis-ci integration.
    Relates to #23.

--- tests/test.js
+++ tests/test.js
@@ -10,4 +10,7 @@ test.print = function(print) {
 		print(test.failures[i].toString())
 	}
 	print("tests: " + test.total + "\nfailures: " + test.failures.length)
+  if (test.failures.length > 0) {
+    throw new Error(test.failures.length + " test did not pass.")
+  }
 }
