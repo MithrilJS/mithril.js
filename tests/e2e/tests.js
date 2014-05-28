85f754cd3a1f01fbd477b98a0167959a8a39de58
Sergey Kirillov <sergey.kirillov@gmail.com>
2014-05-28 23:54:17 +0300

    Added assert messages

--- tests/e2e/tests.js
+++ tests/e2e/tests.js
@@ -6,7 +6,8 @@ test('Mithril accessible as window.m', function() {
 });
 
 
-test('issue99', function() {
+test('issue99 regression', function() {
+	// https://github.com/lhorie/mithril.js/issues/99
 	expect(3);
 	var view1 = m('div', {}, [
 		m('a', {href: '#/product/' + 12345}, 'link1')
@@ -19,12 +20,12 @@ test('issue99', function() {
 	]);
 
 	m.render(dummyEl, view1);
-	equal(dummyEl.innerHTML, '<div><a href="#/product/12345">link1</a></div>');
+	equal(dummyEl.innerHTML, '<div><a href="#/product/12345">link1</a></div>', 'view1 rendered correctly');
 
 	m.render(dummyEl, view2);
-	equal(dummyEl.innerHTML, '<div><img src="http://www.ru" align="right" width="400"><h1>Product1</h1><h2>brand</h2></div>');
+	equal(dummyEl.innerHTML, '<div><img src="http://www.ru" align="right" width="400"><h1>Product1</h1><h2>brand</h2></div>', 'view2 rendered correctly');
 
 	m.render(dummyEl, view1);
-	equal(dummyEl.innerHTML, '<div><a href="#/product/12345">link1</a></div>');
+	equal(dummyEl.innerHTML, '<div><a href="#/product/12345">link1</a></div>', 'view1 should be restored correctly');
 
 });
