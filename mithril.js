bbafe11d465c4599893b4a5bc11b547fef03f1df
zzmp <zmp@umich.edu>
2014-09-19 02:17:13 -0700

    retool tests for new m.route signature
    - Change signatures in tests
    - Update failing tests due to redirection issues (#250)
    - Include m.route.clear for testing

--- mithril.js
+++ mithril.js
@@ -563,6 +563,14 @@ Mithril = m = new function app(window, undefined) {
 			else window.location[m.route.mode] = currentRoute
 		}
 	}
+	m.route.clear = function() {
+		window[m.route.mode == "hash" ? "onhashchange" : "onpopstate"] = noop
+		m.route.mode = "search"
+		redirect = noop
+		router = {}
+		routes = {}
+		defaultModules = {}
+	}
 	m.route.param = function(key) {return routeParams[key]}
 	m.route.mode = "search"
 	function normalizeRoute(route) {return route.slice(modes[m.route.mode].length)}
