99c079dbfcacd35b373c5e6ba2120c78ecb88155
Leo Horie <lhorie@klick.com>
2014-09-25 00:34:16 -0400

    don't send content-type header if no data

--- mithril.js
+++ mithril.js
@@ -857,7 +857,7 @@ Mithril = m = new function app(window, undefined) {
 					else options.onerror({type: "error", target: xhr})
 				}
 			}
-			if (options.serialize == JSON.stringify && options.method != "GET") {
+			if (options.serialize == JSON.stringify && options.data && options.method != "GET") {
 				xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8")
 			}
 			if (typeof options.config == "function") {
