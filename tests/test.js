d6911d105b65317fd9fe46c1b0e1591ff587a689
Leo Horie <leohorie@hotmail.com>
2015-04-21 09:24:13 -0400

    clean up unused test code

--- tests/test.js
+++ tests/test.js
@@ -1,59 +1,17 @@
-//make "use strict" and nodejs happy
-var window = this
-
-//test reporting for saucelabs
-if (typeof window != "undefined") {
-	window.global_test_results = {
-		tests: [],
-		duration: 0,
-		passed: 0,
-		failed: 0
-	};
-}
-
 if (!this.console) {
 	var log = function(value) {document.write("<pre>" + value + "</pre>")}
 	this.console = {log: log, error: log}
 }
 
 function test(condition) {
-	var duration = 0
-	var start = 0
-	var result = true
 	test.total++
 
-	if (typeof performance != "undefined" && performance.now) {
-		start = performance.now()
-	}
 	try {
 		if (!condition()) throw new Error("failed")
 	}
 	catch (e) {
-		result = false
 		console.error(e)
 		test.failures.push(condition)
-	}
-	if (typeof performance != "undefined" && performance.now) {
-		duration = performance.now() - start
-	}
-
-	window.test_obj = {
-		name: "" + test.total,
-		result: result,
-		duration: duration
-	}
-
-	if (typeof window != "undefined") {
-		if (!result) {
-			window.global_test_results.tests.push(window.test_obj)
-		}
-
-		window.global_test_results.duration += duration
-		if (result) {
-			window.global_test_results.passed++
-		} else {
-			window.global_test_results.failed++
-		}
 	}
 }
 test.total = 0
