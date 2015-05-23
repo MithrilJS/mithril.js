689a080fd56037f17fb12231c1d3be7ffebe01b8
Barney <barney.carroll@gmail.com>
2015-05-23 14:05:17 +0100

    Don't presume that custom attributes should be removed post-transformation. Simplify match.

--- mithril.js
+++ mithril.js
@@ -57,7 +57,7 @@ var m = (function app(window, undefined) {
 		else {
 			cell.children = children
 		}
-		
+
 		for (var attrName in attrs) {
 			if (attrs.hasOwnProperty(attrName)) {
 				if (attrName === classAttrName && attrs[attrName] != null && attrs[attrName] !== "") {
@@ -69,15 +69,10 @@ var m = (function app(window, undefined) {
 		}
 		if (classes.length > 0) cell.attrs[classAttrName] = classes.join(" ");
 
-
 		// loop over output for custom attribute transformations
 		for (attrName in cell.attrs){
-			if (attrs.hasOwnProperty(attrName) && attrName in m.attrs){
-				var value = cell.attrs[attrName]
-
-				delete cell.attrs[attrName]
-
-				var transformed = m.attrs[attrName](cell, value)
+			if (attrName in m.attrs){
+				var transformed = m.attrs[attrName](cell, cell.attrs[attrName])
 
 				if (transformed !== undefined) cell = transformed
 			}
@@ -138,7 +133,7 @@ var m = (function app(window, undefined) {
 					len = data.length
 				}
 			}
-			
+
 			var nodes = [], intact = cached.length === data.length, subArrayCount = 0;
 
 			//keys algorithm: sort elements without recreating them if keys are present
@@ -154,7 +149,7 @@ var m = (function app(window, undefined) {
 					existing[cached[i].attrs.key] = {action: DELETION, index: i}
 				}
 			}
-			
+
 			var guid = 0
 			for (var i = 0, len = data.length; i < len; i++) {
 				if (data[i] && data[i].attrs && data[i].attrs.key != null) {
@@ -164,7 +159,7 @@ var m = (function app(window, undefined) {
 					break
 				}
 			}
-			
+
 			if (shouldMaintainIdentities) {
 				var keysDiffer = false
 				if (data.length != cached.length) keysDiffer = true
@@ -174,7 +169,7 @@ var m = (function app(window, undefined) {
 						break
 					}
 				}
-				
+
 				if (keysDiffer) {
 					for (var i = 0, len = data.length; i < len; i++) {
 						if (data[i] && data[i].attrs) {
@@ -238,7 +233,7 @@ var m = (function app(window, undefined) {
 			}
 			if (!intact) {
 				//diff the array itself
-				
+
 				//update the list of DOM nodes by collecting the nodes from each item
 				for (var i = 0, len = data.length; i < len; i++) {
 					if (cached[i] != null) nodes.push.apply(nodes, cached[i].nodes)
@@ -291,7 +286,7 @@ var m = (function app(window, undefined) {
 			if (data.attrs.xmlns) namespace = data.attrs.xmlns;
 			else if (data.tag === "svg") namespace = "http://www.w3.org/2000/svg";
 			else if (data.tag === "math") namespace = "http://www.w3.org/1998/Math/MathML";
-			
+
 			if (isNew) {
 				if (data.attrs.is) node = namespace === undefined ? $document.createElement(data.tag, data.attrs.is) : $document.createElementNS(namespace, data.tag, data.attrs.is);
 				else node = namespace === undefined ? $document.createElement(data.tag) : $document.createElementNS(namespace, data.tag);
@@ -316,7 +311,7 @@ var m = (function app(window, undefined) {
 						}
 					}
 				}
-				
+
 				if (cached.children && !cached.children.nodes) cached.children.nodes = [];
 				//edge case: setting value on <select> doesn't work before children exist, so set it again after children have been created
 				if (data.tag === "select" && "value" in data.attrs) setAttributes(node, data.tag, {value: data.attrs.value}, {}, namespace);
@@ -583,7 +578,7 @@ var m = (function app(window, undefined) {
 		if (!root) throw new Error("Please ensure the DOM element exists before rendering a template into it.");
 		var index = roots.indexOf(root);
 		if (index < 0) index = roots.length;
-		
+
 		var isPrevented = false;
 		var event = {preventDefault: function() {
 			isPrevented = true;
@@ -597,11 +592,11 @@ var m = (function app(window, undefined) {
 			for (var i = 0, unloader; unloader = unloaders[i]; i++) unloader.controller.onunload = unloader.handler
 		}
 		else unloaders = []
-		
+
 		if (controllers[index] && typeof controllers[index].onunload === FUNCTION) {
 			controllers[index].onunload(event)
 		}
-		
+
 		if (!isPrevented) {
 			m.redraw.strategy("all");
 			m.startComputation();
@@ -842,7 +837,7 @@ var m = (function app(window, undefined) {
 	}
 	function parseQueryString(str) {
 		if (str.charAt(0) === "?") str = str.substring(1);
-		
+
 		var pairs = str.split("&"), params = {};
 		for (var i = 0, len = pairs.length; i < len; i++) {
 			var pair = pairs[i].split("=");
@@ -858,7 +853,7 @@ var m = (function app(window, undefined) {
 	}
 	m.route.buildQueryString = buildQueryString
 	m.route.parseQueryString = parseQueryString
-	
+
 	function reset(root) {
 		var cacheKey = getCellCacheKey(root);
 		clear(root.childNodes, cellCache[cacheKey]);
