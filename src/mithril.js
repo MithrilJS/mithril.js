a0ea4c5b66f77e3556c38f656956e0b80e9a3a51
Pat Cavit <pcavit@gmail.com>
2015-12-21 15:03:49 -0800

    Expose m.render
    Also re-order and actually call initialize

--- src/mithril.js
+++ src/mithril.js
@@ -1,11 +1,16 @@
-import {prop} from "./prop.js";
-import {request} from "./request.js";
-import {redraw} from "./redraw.js";
-import {route} from "./route.js";
-import {mount} from "./mount.js";
+import {initialize} from "./env.js";
+
 import {component} from "./component.js";
-import {trust} from "./trust.js";
+import {mount} from "./mount.js";
 import {parse} from "./parse.js";
+import {prop} from "./prop.js";
+import {redraw} from "./redraw.js";
+import {render} from "./render.js";
+import {request} from "./request.js";
+import {route} from "./route.js";
+import {trust} from "./trust.js";
+
+initialize(window);
 
 var m = parse;
 
@@ -13,12 +18,13 @@ m.version = function() {
     return "v0.2.3";
 };
 
+m.component = component;
 m.mount = mount;
 m.prop = prop;
 m.redraw = redraw;
+m.render = render;
 m.request = request;
 m.route = route;
 m.trust = trust;
-m.component = component;
 
 export default m;
