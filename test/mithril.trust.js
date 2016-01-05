2cc898877dcc9aaabccbd876cea918b28d25357e
John Long <nakaroka@gmail.com>
2016-01-05 14:25:52 -0700

    Possibly fixes trust tests

--- test/mithril.trust.js
+++ test/mithril.trust.js
@@ -20,19 +20,19 @@ describe("m.trust()", function () {
 	// FIXME: implement document.createRange().createContextualFragment() in the
 	// mock window for these tests
 	dom(function () {
-		xit("isn't escaped in m.render()", function () {
+		it("isn't escaped in m.render()", function () {
 			var root = document.createElement("div")
 			m.render(root, m("div", "a", m.trust("&amp;"), "b"))
 			expect(root.childNodes[0].innerHTML).to.equal("a&amp;b")
 		})
 
-		xit("works with mixed trusted content in div", function () {
+		it("works with mixed trusted content in div", function () {
 			var root = document.createElement("div")
 			m.render(root, [m.trust("<p>1</p><p>2</p>"), m("i", "foo")])
 			expect(root.childNodes[2].tagName).to.equal("I")
 		})
 
-		xit("works with mixed trusted content in text nodes", function () {
+		it("works with mixed trusted content in text nodes", function () {
 			var root = document.createElement("div")
 			m.render(root, [
 				m.trust("<p>1</p>123<p>2</p>"),
@@ -43,7 +43,7 @@ describe("m.trust()", function () {
 
 		// FIXME: this is a bug (trusted string's contents rendered as just
 		// textual contents)
-		xit("works with mixed trusted content in td", function () {
+		it("works with mixed trusted content in td", function () {
 			var root = document.createElement("table")
 			root.appendChild(root = document.createElement("tr"))
 
