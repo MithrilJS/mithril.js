55b99dd0e2e320dc549f499d972a1da4f82e337c
impinball <impinball@gmail.com>
2016-03-02 13:44:42 -0500

    Remove an erroneous npm-debug.log, disable failing test
    Driveby fix: fix a few linter warnings and actually have CI linting things.

--- test/mithril.deferred.js
+++ test/mithril.deferred.js
@@ -293,17 +293,18 @@ describe("m.deferred()", function () {
 		prmA.resolve("A")
 		prmB.resolve("B")
 
-		prmA.promise.then(function (A) {
+		prmA.promise.then(function () {
 			return prmB.promise
-		}).then(function(B) {
+		}).then(function (B) {
 			expect(B).to.equal("B")
 		})
 	})
+
 	it("yields immutable promises", function () {
 		var d = m.deferred()
 		d.resolve(5)
 		d.resolve(6)
-		d.promise.then(function(v) {
+		d.promise.then(function (v) {
 			expect(v).to.equal(5)
 		})
 	})
