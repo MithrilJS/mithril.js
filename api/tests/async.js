0cf509f674ea3899293450b45108728c52a25d2e
Pat Cavit <pcavit@gmail.com>
2016-05-18 23:08:35 -0700

    Add forcing support and improve tests
    Also verify that we're using tabs, whee.

--- api/tests/async.js
+++ api/tests/async.js
@@ -1,12 +1,46 @@
+var _fns = []
+var _last  = 0
+var _frame = 1000 / 60
+
 module.exports = {
     setTimeout : function($window) {
-        $window.setTimeout = window.setTimeout;
-        $window.clearTimeout = window.clearTimeout;
+        $window.setTimeout = typeof window === "object" ? window.setTimeout : global.setTimeout;
+        $window.clearTimeout = typeof window === "object" ? window.clearTimeout : global.setTimeout;
     },
 
     requestAnimationFrame : function($window) {
-        $window.requestAnimationFrame = window.requestAnimationFrame;
-        $window.cancelAnimationFrame = window.cancelAnimationFrame;
+        // Modified version of https://github.com/chrisdickinson/raf
+        // Copyright chrisdickinson I guess?
+        $window.requestAnimationFrame = typeof window === "object" ? window.requestAnimationFrame : function(fn) {
+            if(!_fns.length) {
+                var now = Date.now()
+                var next = Math.max(0, _frame - (now - _last))
+                
+                _last = next + now
+                
+                setTimeout(function() {
+                    var fns = _fns.slice()
+                    
+                    _fns = []
+                    
+                    for(var i = 0; i < fns.length; i++) {
+                        if(typeof fns[i] !== "function") {
+                            continue
+                        }
+                        
+                        fns[i](_last)
+                    }
+                }, Math.round(next))
+            }
+            
+            _fns.push(fn)
+            
+            return _fns.length - 1;
+        }
+        
+        $window.cancelAnimationFrame = typeof window === "object" ? window.cancelAnimationFrame : function(handle) {
+            _fns[handle] = null
+        };
     }
 }
 
--- api/tests/test-limiter.js
+++ api/tests/test-limiter.js
@@ -7,23 +7,63 @@ var async = require("./async")
 var limiter = require("../limiter")
 
 o.spec("fps limiter", function() {
-    var $window, root
+	var $window, root
 
-    [ "setTimeout", "requestAnimationFrame" ].forEach(function(type) {
-        o.spec(type, function() {
-            o.beforeEach(function() {
-                $window = domMock()
-                
-                async[type]($window)
-            })
-            
-            o("is a function", function() {
-                o(typeof limiter).equals("function")
-            })
-            
-            o("it returns a function", function() {
-                o(typeof limiter(false)).equals("function")
-            })
-        })
-    })
+	[
+		"setTimeout",
+		"requestAnimationFrame",
+	].forEach(function(type) {
+		o.spec(type, function() {
+			o.beforeEach(function() {
+				$window = domMock()
+				
+				async[type]($window)
+			})
+			
+			o("is a function", function() {
+				o(typeof limiter).equals("function")
+			})
+			
+			o("it returns a function", function() {
+				o(typeof limiter($window, false)).equals("function")
+			})
+			
+			o("it runs synchronously the first time", function() {
+				var spy = o.spy()
+				var run = limiter($window, spy)
+				
+				run()
+				
+				o(spy.callCount).equals(1)
+			})
+			
+			o("it only runs once per tick", function(done) {
+				var spy = o.spy()
+				var run = limiter($window, spy)
+				
+				run()
+				run()
+				run()
+				
+				o(spy.callCount).equals(1)
+				
+				setTimeout(function() {
+					o(spy.callCount).equals(2)
+					
+					done()
+				}, 17)
+			})
+			
+			o("it supports forcing a synchronous redraw", function() {
+				var spy = o.spy()
+				var run = limiter($window, spy)
+				
+				run()
+				run()
+				run(true)
+				
+				o(spy.callCount).equals(2)
+			})
+		})
+	})
 })
