bf85b7a530dfacc255cb0fe78a5a0f00df016a82
Leo Horie <leohorie@hotmail.com>
2016-05-26 11:04:04 -0400

    #1076 absorb native promise in m.prop

--- mithril.js
+++ mithril.js
@@ -1306,7 +1306,7 @@
 	}
 
 	m.prop = function (store) {
-		if ((store != null && isObject(store) || isFunction(store)) &&
+		if ((store != null && isObject(store) || isFunction(store)) || store instanceof window.Promise &&
 				isFunction(store.then)) {
 			return propify(store)
 		}
