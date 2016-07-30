47b9aba68050230eeff0754f4076b7596cbe9eb9
Pat Cavit <github@patcavit.com>
2016-07-29 23:18:11 -0700

    Use Date.now() if available since it's faster

--- api/throttle.js
+++ api/throttle.js
@@ -1,12 +1,16 @@
 "use strict"
 
+var ts = Date.now || function() {
+	return new Date().getTime()
+}
+
 module.exports = function(callback) {
 	//60fps translates to 16.6ms, round it down since setTimeout requires int
 	var time = 16
 	var last = 0, pending = null
 	var timeout = typeof requestAnimationFrame === "function" ? requestAnimationFrame : setTimeout
 	return function(synchronous) {
-		var now = new Date().getTime()
+		var now = ts()
 		if (synchronous === true || last === 0 || now - last >= time) {
 			last = now
 			callback()
@@ -15,7 +19,7 @@ module.exports = function(callback) {
 			pending = timeout(function() {
 				pending = null
 				callback()
-				last = new Date().getTime()
+				last = ts()
 			}, time - (now - last))
 		}
 	}
