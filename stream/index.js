4854b4f959f5c4346542aac849b287895e093caa
Sebastian Sandqvist <seb@sparque.me>
2016-08-15 12:10:51 -0700

    remove semicolons

--- stream/index.js
+++ stream/index.js
@@ -1,13 +1,13 @@
-"use strict";
+"use strict"
 
 var log = console.error.bind(console)
 var StreamFactory = require("../util/stream")
 var Stream = StreamFactory(log)
 
-var stream = Stream.stream;
+var stream = Stream.stream
 stream.combine = Stream.combine
 stream.reject = Stream.reject
 stream.merge = Stream.merge
 stream.HALT = Stream.HALT
 
-module.exports = stream;
+module.exports = stream
