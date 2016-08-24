ee27a8bab8891d54e5f351434bc69d6438794367
Leo Horie <leohorie@hotmail.com>
2016-08-24 01:20:27 -0400

    fix typo in comment

--- bundler/minify.js
+++ bundler/minify.js
@@ -13,7 +13,7 @@ module.exports = function(input, output, options) {
 		var data = {
 			output_format: "json",
 			output_info: ["compiled_code", "warnings", "errors", "statistics"],
-			compilation_level: "SIMPLE_OPTIMIZATIONS",//ADVANDED_OPTIMIZATIONS
+			compilation_level: "SIMPLE_OPTIMIZATIONS",//"ADVANCED_OPTIMIZATIONS",
 			warning_level: "default",
 			output_file_name: "default.js",
 			js_code: code,
