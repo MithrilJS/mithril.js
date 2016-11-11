717186a3b63b5f66a5fbfd2495da1d2147835d13
Leo Horie <leohorie@hotmail.com>
2016-11-11 17:57:16 -0500

    clean up

--- request/request.js
+++ request/request.js
@@ -1,7 +1,6 @@
 "use strict"
 
 var buildQueryString = require("../querystring/build")
-var Promise = require("../promise/promise")
 
 module.exports = function($window, Stream) {
 	var callbackCount = 0
