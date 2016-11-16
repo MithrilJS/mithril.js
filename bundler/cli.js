93a130b8b40f65955aa66c2eaecff040df6482c9
Pat Cavit <github@patcavit.com>
2016-11-16 00:27:51 -0800

    Update README size w/ minified builds

--- bundler/cli.js
+++ bundler/cli.js
@@ -1,5 +1,7 @@
 "use strict"
 
+var fs = require("fs");
+
 var bundle = require("./bundle")
 var minify = require("./minify")
 
@@ -23,4 +25,25 @@ function add(value) {
 }
 
 bundle(params.input, params.output, {watch: params.watch})
-if (params.minify) minify(params.output, params.output, {watch: params.watch, advanced: params.aggressive})
+if (params.minify) {
+	minify(params.output, params.output, {watch: params.watch, advanced: params.aggressive}, function(stats) {
+		var readme, kb;
+
+		function format(n) {
+			return n.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+		}
+
+		console.log("Original size: " + format(stats.originalGzipSize) + " bytes gzipped (" + format(stats.originalSize) + " bytes uncompressed)")
+		console.log("Compiled size: " + format(stats.compressedGzipSize) + " bytes gzipped (" + format(stats.compressedSize) + " bytes uncompressed)")
+
+		readme = fs.readFileSync("./README.md", "utf8")
+		kb = stats.compressedGzipSize / 1024
+
+		fs.writeFileSync("./README.md",
+			readme.replace(
+				/(<!-- size -->)(.+?)(<!-- \/size -->)/,
+				"$1" + (kb % 1 ? kb.toFixed(2) : kb) + " KB$3"
+			)
+		)
+	})
+}
