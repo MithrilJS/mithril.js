605f4408cb6b943e34566493735ef70f8b0f218a
Barney Carroll <barney.carroll@gmail.com>
2016-11-19 18:23:30 +0000

    Allow line breaks in hyperscript selector attribute values

--- render/hyperscript.js
+++ render/hyperscript.js
@@ -2,7 +2,7 @@
 
 var Vnode = require("../render/vnode")
 
-var selectorParser = /(?:(^|#|\.)([^#\.\[\]]+))|(\[\s*(.+?)(?:\s*=\s*("|'|)((?:\\["'\]]|.)*?)\5)?\s*\])/g
+var selectorParser = /(?:(^|#|\.)([^#\.\[\]]+))|(\[\s*(.+?)(?:\s*=\s*("|'|)((?:\\["'\]]|.|\n|\r)*?)\5)?\s*\])/g
 var selectorCache = {}
 function hyperscript(selector) {
 	if (selector == null || typeof selector !== "string" && selector.view == null) {
