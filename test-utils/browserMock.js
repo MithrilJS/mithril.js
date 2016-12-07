bd792979054ad7b27056312485368b7a4f34a434
Pierre-Yves Gerardy <py@py.gy>
2016-12-07 13:42:27 +0100

    The browserMock $window must be reachable from the pushStateMock

--- test-utils/browserMock.js
+++ test-utils/browserMock.js
@@ -5,14 +5,14 @@ var domMock = require("./domMock")
 var xhrMock = require("./xhrMock")
 
 module.exports = function(env) {
-	var $window = {}
+	env = env || {}
+	var $window = env.window = {}
 
 	var dom = domMock()
 	var xhr = xhrMock()
-	var ps = pushStateMock(env)
 	for (var key in dom) if (!$window[key]) $window[key] = dom[key]
 	for (var key in xhr) if (!$window[key]) $window[key] = xhr[key]
-	for (var key in ps) if (!$window[key]) $window[key] = ps[key]
+	pushStateMock(env)
 
 	return $window
 }
