3cde52612611d270817a9fe93bba693ffadbb586
Pierre-Yves Gerardy <py@py.gy>
2017-01-19 14:22:17 +0100

    [ospec:cli] test to check that the CLI tests are not run as part of the main suite

--- ospec/bin/cli-spec/tests/dummy.js
+++ ospec/bin/cli-spec/tests/dummy.js
@@ -1,5 +1,10 @@
 var o = require('../test-utils/ospec')
 
+var path = require('path')
+
+// ensure that this only runs as part of the `cli-spec` tests.
+o(process.cwd()).equals(path.join(__dirname, '..'))
+
 var runs = 0
 o('runs only once', function(){
 	o(++runs).equals(1)
