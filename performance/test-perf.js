c3adccbca4ab4cf6a38b57266eebee79426d5665
Pat Cavit <github@patcavit.com>
2017-04-07 22:20:21 -0700

    refactor: ES5, check noops after warming up caches
    Also adjusted thresholds up

--- performance/test-perf.js
+++ performance/test-perf.js
@@ -29,14 +29,22 @@ SOFTWARE.
 var o = require("../ospec/ospec")
 var browserMock = require("../test-utils/browserMock")
 
-var now = typeof performance!=="undefined" && performance.now ? () => performance.now() : () => Number(new Date());
+var now = typeof performance!=="undefined" && performance.now ? function () { return performance.now(); } : function () { return Date.now(); };
 
-function verify(name, threshold, result, done) {
-	console.log(`PERF: ${name}: ${Math.floor(result.hz)}/s (${result.ticks} ticks, ${threshold} allowed)`);
+function verify(name, threshold, done) {
+	return function(result) {
+		console.log(
+			name + ": " +
+			Math.floor(result.hz) + "/s (" +
+				result.ticks + " ticks, " +
+				Math.floor((result.ticks / threshold) * 100) + "% of threshold" +
+			")"
+		);
 
-	o(result.ticks <= threshold).equals(true);
+		o(result.ticks <= threshold).equals(true)(result.ticks + " ticks, " + threshold + " allowed");
 
-	done();
+		done();
+	};
 }
 
 function loop(iter, time) {
@@ -57,34 +65,37 @@ function benchmark(iter, callback) {
 		count = 5,
 		time = 500,
 		passes = 0,
-		noops = loop(noop, time),
-		iterations = 0,
-		i;
+		total = 0,
+		noops, i;
 
 	function noop() {
 		try { a++; } finally { a += Math.random(); }
 	}
 
 	// warm
-	for(i=100; i--;) noop(), iter();
+	for(i=100; i--;) {
+		noop();
+		iter();
+	}
 
+	// Count how many noops() occur in the time period
+	noops = loop(noop, time);
+
+	// run iter() through loop() `count` times
 	function next() {
-		iterations += loop(iter, time);
+		total += loop(iter, time);
 
 		setTimeout(++passes === count ? done : next, 10);
 	}
 
 	function done() {
-		var ticks = Math.round(noops / iterations * count),
-			hz = iterations / count / time * 1000;
-
 		callback({
-			iterations : iterations,
+			total : total,
 			noops : noops,
 			count : count,
-			time : time,
-			ticks : ticks,
-			hz : hz
+			time  : time,
+			ticks : Math.round(noops / total * count),
+			hz    : total / count / time * 1000
 		});
 	}
 
@@ -94,30 +105,31 @@ function benchmark(iter, callback) {
 o.spec("perf", function() {
 	var m, scratch
 
-	o.before(() => {
+	o.before(function () {
 		var doc = typeof document !== "undefined" ? document : null;
 
 		if(!doc) {
 			var mock = browserMock()
-			if (typeof global !== "undefined") global.window = mock
+			if (typeof global !== "undefined") { global.window = mock }
 
 			doc = mock.document;
 		}
+
 		m = require("../mithril") // eslint-disable-line global-require
 
 		scratch = doc.createElement("div");
 		(doc.body || doc.documentElement).appendChild(scratch);
 	})
 
-	o.afterEach(() => {
+	o.afterEach(function () {
 		scratch.innerHTML = "";
 	})
 
-	o.after(() => {
+	o.after(function () {
 		scratch = null
 	})
 
-	o("rerender without changes", (done, timeout) => {
+	o("rerender without changes", function (done, timeout) {
 		timeout(5000);
 
 		var vdom = m("div", {"class": "foo bar", "data-foo": "bar", p: 2},
@@ -163,105 +175,121 @@ o.spec("perf", function() {
 		);
 
 		benchmark(
-			() => m.render(scratch, vdom),
-			(out) => {
-				verify("rerender without changes", 5, out, done)
-			}
+			function () {
+				m.render(scratch, vdom);
+			},
+			verify("rerender without changes", 5, done)
 		);
 	});
 
-	o("repeated trees", (done, timeout) => {
+	o("repeated trees", function (done, timeout) {
 		timeout(5000);
 
 		var Header = {
-			view : () => m("header",
-				m("h1", {"class": "asdf"}, "a ", "b", " c ", 0, " d"),
-				m("nav",
-					m("a", {href: "/foo"}, "Foo"),
-					m("a", {href: "/bar"}, "Bar")
-				)
-			)
+			view : function () {
+				return m("header",
+					m("h1", {"class": "asdf"}, "a ", "b", " c ", 0, " d"),
+					m("nav",
+						m("a", {href: "/foo"}, "Foo"),
+						m("a", {href: "/bar"}, "Bar")
+					)
+				);
+			}
 		}
 
 		var Form = {
-			view : () => m("form", {onSubmit: function onSubmit() {}},
-				m("input", {type: "checkbox", checked: true}),
-				m("input", {type: "checkbox", checked: false}),
-				m("fieldset",
-					m("label",
-						m("input", {type: "radio", checked: true})
+			view : function () {
+				return m("form", {onSubmit: function onSubmit() {}},
+					m("input", {type: "checkbox", checked: true}),
+					m("input", {type: "checkbox", checked: false}),
+					m("fieldset",
+						m("label",
+							m("input", {type: "radio", checked: true})
+						),
+						m("label",
+							m("input", {type: "radio"})
+						)
 					),
-					m("label",
-						m("input", {type: "radio"})
-					)
-				),
-				m(ButtonBar, null)
-			)
+					m(ButtonBar, null)
+				);
+			}
 		}
 
 		var ButtonBar = {
-			view : () => m("button-bar",
-				m(Button,
-					{style: "width:10px; height:10px; border:1px solid #FFF;"},
-					"Normal CSS"
-				),
-				m(Button,
-					{style: "top:0 ; right: 20"},
-					"Poor CSS"
-				),
-				m(Button,
-					{style: "invalid-prop:1;padding:1px;font:12px/1.1 arial,sans-serif;", icon: true},
-					"Poorer CSS"
-				),
-				m(Button,
-					{style: {margin: 0, padding: "10px", overflow: "visible"}},
-					"Object CSS"
-				)
-			)
+			view : function () {
+				return m("button-bar",
+					m(Button,
+						{style: "width:10px; height:10px; border:1px solid #FFF;"},
+						"Normal CSS"
+					),
+					m(Button,
+						{style: "top:0 ; right: 20"},
+						"Poor CSS"
+					),
+					m(Button,
+						{style: "invalid-prop:1;padding:1px;font:12px/1.1 arial,sans-serif;", icon: true},
+						"Poorer CSS"
+					),
+					m(Button,
+						{style: {margin: 0, padding: "10px", overflow: "visible"}},
+						"Object CSS"
+					)
+				);
+			}
 		}
 
 		var Button = {
-			view : (vnode) => m("button", vnode.attrs, vnode.children)
+			view : function (vnode) {
+				return m("button", vnode.attrs, vnode.children);
+			}
 		}
 
 		var Main = {
-			view : () => m(Form)
+			view : function () {
+				return m(Form);
+			}
 		}
 
 		var Root = {
-			view : () => m("div",
-				{"class": "foo bar", "data-foo": "bar", p: 2},
-				m(Header, null),
-				m(Main, null)
-			)
+			view : function () {
+				return m("div",
+					{"class": "foo bar", "data-foo": "bar", p: 2},
+					m(Header, null),
+					m(Main, null)
+				);
+			}
 		}
 
 		var Empty = {
-			view : () => m("div")
+			view : function () {
+				return m("div");
+			}
 		}
 
 		var Parent = {
-			view : (vnode) => m(vnode.attrs.child)
+			view : function (vnode) {
+				return m(vnode.attrs.child);
+			}
 		}
 
 		benchmark(
-			() => {
+			function () {
 				m.render(scratch, m(Parent, {child : Root}));
 				m.render(scratch, m(Parent, {child : Empty}));
 			},
-			(out) => {
-				verify("repeated trees", 1200, out, done);
-			}
+			verify("repeated trees", 3300, done)
 		);
 	});
 
-	o("large VDOM tree", (done, timeout) => {
+	o("construct large VDOM tree", function (done, timeout) {
 		timeout(5000);
 
 		var fields = [],
 			out = [];
 
-		for(let i=100; i--;) fields.push((i*999).toString(36));
+		for(var i=100; i--;) {
+			fields.push((i * 999).toString(36));
+		}
 
 		function digest(vnode) {
 			out.push(vnode);
@@ -269,72 +297,75 @@ o.spec("perf", function() {
 		}
 
 		benchmark(
-			() => digest(
-				m("div", {"class": "foo bar", "data-foo": "bar", p: 2},
-					m("header",
-						m("h1", {"class": "asdf"}, "a ", "b", " c ", 0, " d"),
-						m("nav",
-							m("a", {href: "/foo"}, "Foo"),
-							m("a", {href: "/bar"}, "Bar")
-						)
-					),
-					m("main",
-						m("form",
-							{onSubmit: function onSubmit() {}},
-							m("input", {type: "checkbox", checked: true}),
-							m("input", {type: "checkbox"}),
-							m("fieldset",
-								fields.map((field) => m("label",
-									field,
-									":",
-									m("input", {placeholder: field})
-								))
-							),
-							m("button-bar",
-								m("button",
-									{style: "width:10px; height:10px; border:1px solid #FFF;"},
-									"Normal CSS"
+			function () {
+				return digest(
+					m("div", {"class": "foo bar", "data-foo": "bar", p: 2},
+						m("header",
+							m("h1", {"class": "asdf"}, "a ", "b", " c ", 0, " d"),
+							m("nav",
+								m("a", {href: "/foo"}, "Foo"),
+								m("a", {href: "/bar"}, "Bar")
+							)
+						),
+						m("main",
+							m("form",
+								{onSubmit: function onSubmit() {}},
+								m("input", {type: "checkbox", checked: true}),
+								m("input", {type: "checkbox"}),
+								m("fieldset",
+									fields.map(function (field) {
+										return m("label",
+											field,
+											":",
+											m("input", {placeholder: field})
+										);
+									})
 								),
-								m("button",
-									{style: "top:0 ; right: 20"},
-									"Poor CSS"
-								),
-								m("button",
-									{style: "invalid-prop:1;padding:1px;font:12px/1.1 arial,sans-serif;", icon: true},
-									"Poorer CSS"
-								),
-								m("button",
-									{style: {margin: 0, padding: "10px", overflow: "visible"}},
-									"Object CSS"
+								m("button-bar",
+									m("button",
+										{style: "width:10px; height:10px; border:1px solid #FFF;"},
+										"Normal CSS"
+									),
+									m("button",
+										{style: "top:0 ; right: 20"},
+										"Poor CSS"
+									),
+									m("button",
+										{style: "invalid-prop:1;padding:1px;font:12px/1.1 arial,sans-serif;", icon: true},
+										"Poorer CSS"
+									),
+									m("button",
+										{style: {margin: 0, padding: "10px", overflow: "visible"}},
+										"Object CSS"
+									)
 								)
 							)
 						)
 					)
-				)),
-			(out) => {
-				verify("large VDOM tree", 700, out, done);
-			}
+				);
+			},
+			verify("construct large VDOM tree", 2500, done)
 		);
 	})
 
-	o("mutate styles/properties", (done, timeout) => {
+	o("mutate styles/properties", function (done, timeout) {
 		timeout(5000);
 
 		var counter = 0
-		var keyLooper = (n) => (c) => c % n ? `${c}px` : c
-		var get = (obj, i) => obj[i%obj.length]
+		var keyLooper = function (n) { return function (c) { return c % n ? (c + "px") : c; }; }
+		var get = function (obj, i) { return obj[i%obj.length]; }
 		var classes = ["foo", "foo bar", "", "baz-bat", null]
 		var styles = []
 		var multivalue = ["0 1px", "0 0 1px 0", "0", "1px", "20px 10px", "7em 5px", "1px 0 5em 2px"]
 		var stylekeys = [
 			["left", keyLooper(3)],
 			["top", keyLooper(2)],
-			["margin", (c) => get(multivalue, c).replace("1px", c+"px")],
-			["padding", (c) => get(multivalue, c)],
-			["position", (c) => c%5 ? c%2 ? "absolute" : "relative" : null],
-			["display", (c) => c%10 ? c%2 ? "block" : "inline" : "none"],
-			["color", (c) => `rgba(${c%255}, ${255 - c%255}, ${50+c%150}, ${c%50/50})`],
-			["border", (c) => c%5 ? `${c%10}px ${c%2?"solid":"dotted"} ${stylekeys[6][1](c)}` : ""]
+			["margin", function (c) { return get(multivalue, c).replace("1px", c+"px"); }],
+			["padding", function (c) { return get(multivalue, c); }],
+			["position", function (c) { return c%5 ? c%2 ? "absolute" : "relative" : null; }],
+			["display", function (c) { return c%10 ? c%2 ? "block" : "inline" : "none"; }],
+			["color", function (c) { return ("rgba(" + (c%255) + ", " + (255 - c%255) + ", " + (50+c%150) + ", " + (c%50/50) + ")"); }],
+			["border", function (c) { return c%5 ? ((c%10) + "px " + (c%2?"solid":"dotted") + " " + (stylekeys[6][1](c))) : ""; }]
 		]
 		var count = 0
 		var i, j, style, conf, app
@@ -348,23 +379,25 @@ o.spec("perf", function() {
 			styles[i] = style;
 		}
 
-		app = (index) => m("div",
-			{"class": get(classes, index), "data-index": index, title: index.toString(36)},
-			m("input", {type: "checkbox", checked: index % 3 == 0}),
-			m("input", {value: "test " + (Math.floor(index / 4)), disabled: index % 10 ? null : true}),
-			m("div", {"class": get(classes, index * 10)},
-				m("p", {style: get(styles, index)}, "p1"),
-				m("p", {style: get(styles, index + 1)}, "p2"),
-				m("p", {style: get(styles, index * 2)}, "p3"),
-				m("p", {style: get(styles, index * 3 + 1)}, "p4")
-			)
-		);
+		app = function (index) {
+			return m("div",
+				{"class": get(classes, index), "data-index": index, title: index.toString(36)},
+				m("input", {type: "checkbox", checked: index % 3 == 0}),
+				m("input", {value: "test " + (Math.floor(index / 4)), disabled: index % 10 ? null : true}),
+				m("div", {"class": get(classes, index * 10)},
+					m("p", {style: get(styles, index)}, "p1"),
+					m("p", {style: get(styles, index + 1)}, "p2"),
+					m("p", {style: get(styles, index * 2)}, "p3"),
+					m("p", {style: get(styles, index * 3 + 1)}, "p4")
+				)
+			);
+		};
 
 		benchmark(
-			() => m.render(scratch, app(++count)),
-			(out) => {
-				verify("mutate styles/properties", 100, out, done);
-			}
+			function () {
+				m.render(scratch, app(++count));
+			},
+			verify("mutate styles/properties", 300, done)
 		);
 	});
 })
