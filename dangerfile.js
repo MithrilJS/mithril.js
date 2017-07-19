548c9dfe994faa06816c020336494479bcb9df5e
Pat Cavit <github@patcavit.com>
2017-07-19 00:04:39 -0700

    feat: use strict and o.only checking
    "use strict" check only a warning atm

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,49 @@
-/* global message, danger */
+/* eslint-disable */
 "use strict";
 
-message("OK, this worked @" + danger.github.pr.user.login)
+var fs   = require("fs"),
+	path = require("path"),
+
+	locater  = require("locater"),
+	pinpoint = require("pinpoint"),
+	dedent  = require("dedent"),
+
+	jsfiles = danger.git.created_files
+		.concat(danger.git.modified_files)
+		.filter((file) => path.extname(file) === ".js");
+
+function link(file, anchor) {
+	var repo = danger.github.pr.head.repo.html_url,
+		ref = danger.github.pr.head.ref;
+
+	return danger.utils.href(`${repo}/blob/${ref}/${file}${anchor || ""}`, file);
+}
+
+// Every JS file should start with "use strict";
+jsfiles
+	.forEach((file) => {
+		var loc = fs.readFileSync(file, "utf8").indexOf(`"use strict";`);
+
+		if(loc === 0) {
+			return;
+		}
+
+		warn(`${link(file, "#L1")} does not declare strict mode immediately`);
+	});
+
+// Be careful of leaving testing shortcuts in the codebase
+jsfiles
+	.filter((file) => file.indexOf("test") > -1)
+	.forEach(file => {
+		var code = fs.readFileSync(file, "utf8"),
+			locs = locater.find("o.only", code);
+
+		locs.forEach((loc) =>
+			fail(dedent(`
+				${link(file, `#L${loc.line}`)} is preventing tests from running.
+				<pre lang="javascript">
+				${pinpoint(code, { line: loc.line, column : loc.cursor })}
+				</pre>
+			`))
+		)
+	});
