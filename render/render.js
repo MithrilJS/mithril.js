c2c0d40a291c028e01e0f2b5e165aa9aa8a413b4
Jackson Nowotny <jackson.nowotny2@cerner.com>
2017-08-22 14:04:24 -0500

    Fixing IE bug causing active element to be null causing render function to throw error

--- render/render.js
+++ render/render.js
@@ -622,7 +622,8 @@ module.exports = function($window) {
 		updateNodes(dom, dom.vnodes, Vnode.normalizeChildren(vnodes), false, hooks, null, namespace === "http://www.w3.org/1999/xhtml" ? undefined : namespace)
 		dom.vnodes = vnodes
 		for (var i = 0; i < hooks.length; i++) hooks[i]()
-		if ($doc.activeElement !== active) active.focus()
+		// document.activeElement can return null in IE https://developer.mozilla.org/en-US/docs/Web/API/Document/activeElement
+		if (active != null && $doc.activeElement !== active) active.focus()
 	}
 
 	return {render: render, setEventCallback: setEventCallback}
