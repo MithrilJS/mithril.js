406a08d8363163b46d4087db5138b2f33e56a594
Rasmus Porsager <rasmus@porsager.com>
2017-09-01 14:02:58 +0200

    Use util.inspect to log in node - fixes #1661

--- ospec/ospec.js
+++ ospec/ospec.js
@@ -222,6 +222,7 @@ module.exports = new function init(name) {
 		results.push(result)
 	}
 	function serialize(value) {
+		if (hasProcess) return require("util").inspect(value)
 		if (value === null || (typeof value === "object" && !(value instanceof Array)) || typeof value === "number") return String(value)
 		else if (typeof value === "function") return value.name || "<anonymous function>"
 		try {return JSON.stringify(value)} catch (e) {return String(value)}
