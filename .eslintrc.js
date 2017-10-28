3fe56bb503edbac9eb8dbb6e7e5a80f4491a068f
spacejack <spacejack@gmail.com>
2017-10-28 18:32:58 -0400

    Revert eslintrc change, return Promise instead of using async/await

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,6 @@ module.exports = {
         "es6": true,
         "node": true
 	},
-	"parserOptions": {
-		"ecmaVersion": 2017,
-	},
     "extends": "eslint:recommended",
     "rules": {
         "accessor-pairs": "error",
