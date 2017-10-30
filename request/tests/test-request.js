4823ce83c5844c321022cac4d0c3a01c09a9e5fa
spacejack <spacejack@gmail.com>
2017-10-29 23:27:27 -0400

    Add test to ensure errors in extract are rejected

--- request/tests/test-request.js
+++ request/tests/test-request.js
@@ -519,7 +519,7 @@ o.spec("xhr", function() {
 				o(e instanceof Error).equals(true)
 			}).then(done)
 		})
-		o("does not reject on error when extract provided", function(done) {
+		o("does not reject on status error code when extract provided", function(done) {
 			mock.$defineRoutes({
 				"GET /item": function() {
 					return {status: 500, responseText: JSON.stringify({message: "error"})}
@@ -530,6 +530,21 @@ o.spec("xhr", function() {
 				extract: function(xhr) {return JSON.parse(xhr.responseText)}
 			}).then(function(data) {
 				o(data.message).equals("error")
+				done()
+			})
+		})
+		o("rejects on error in extract", function(done) {
+			mock.$defineRoutes({
+				"GET /item": function() {
+					return {status: 500, responseText: JSON.stringify({message: "error"})}
+				}
+			})
+			xhr({
+				method: "GET", url: "/item",
+				extract: function() {throw new Error("error")}
+			}).catch(function(e) {
+				o(e instanceof Error).equals(true)
+			}).then(function() {
 				done()
 			})
 		})
