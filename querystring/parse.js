2666f04161fd5c687c2a990b3f209b33c51baafd
alfa <alfa.jpn@gmail.com>
2017-12-10 18:47:04 +0900

    fix #parseQueryString

--- querystring/parse.js
+++ querystring/parse.js
@@ -4,7 +4,7 @@ module.exports = function(string) {
 	if (string === "" || string == null) return {}
 	if (string.charAt(0) === "?") string = string.slice(1)
 
-	var entries = string.split("&"), data = {}, counters = {}
+	var entries = string.split("&"), data = {}
 	for (var i = 0; i < entries.length; i++) {
 		var entry = entries[i].split("=")
 		var key = decodeURIComponent(entry[0])
@@ -18,17 +18,20 @@ module.exports = function(string) {
 		if (key.indexOf("[") > -1) levels.pop()
 		for (var j = 0; j < levels.length; j++) {
 			var level = levels[j], nextLevel = levels[j + 1]
-			var isNumber = nextLevel == "" || !isNaN(parseInt(nextLevel, 10))
+			var isArray = nextLevel == ""
 			var isValue = j === levels.length - 1
-			if (level === "") {
-				var key = levels.slice(0, j).join()
-				if (counters[key] == null) counters[key] = 0
-				level = counters[key]++
+			var levelValue = isValue ? value : isArray ? [] : {}
+
+			if (cursor instanceof Array) {
+				cursor.push(levelValue)
+			} else {
+				if (cursor[level] == null) {
+					cursor[level] = levelValue
+				} else {
+					levelValue = cursor[level]
+				}
 			}
-			if (cursor[level] == null) {
-				cursor[level] = isValue ? value : isNumber ? [] : {}
-			}
-			cursor = cursor[level]
+			cursor = levelValue
 		}
 	}
 	return data
