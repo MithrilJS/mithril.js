505eaa78cdb2f0e64d9f497e166e1948e4216287
Pierre-Yves Gérardy <pygy79@gmail.com>
2018-03-06 16:57:39 +0100

    Merge pull request #2091 from spacejack/promise-finally
    Add finally support to Promise polyfill

--- promise/promise.js
+++ promise/promise.js
@@ -3,10 +3,18 @@
 var PromisePolyfill = require("./polyfill")
 
 if (typeof window !== "undefined") {
-	if (typeof window.Promise === "undefined") window.Promise = PromisePolyfill
+	if (typeof window.Promise === "undefined") {
+		window.Promise = PromisePolyfill
+	} else if (!window.Promise.prototype.finally) {
+		window.Promise.prototype.finally = PromisePolyfill.prototype.finally
+	}
 	module.exports = window.Promise
 } else if (typeof global !== "undefined") {
-	if (typeof global.Promise === "undefined") global.Promise = PromisePolyfill
+	if (typeof global.Promise === "undefined") {
+		global.Promise = PromisePolyfill
+	} else if (!global.Promise.prototype.finally) {
+		global.Promise.prototype.finally = PromisePolyfill.prototype.finally
+	}
 	module.exports = global.Promise
 } else {
 	module.exports = PromisePolyfill
