e6b3719169b4c3cade2f74e21bd7f0ca110ecaa4
Scotty Simpson <portrayme@mac.com>
2018-11-06 08:05:58 -0800

    Update minify.js

--- bundler/minify.js
+++ bundler/minify.js
@@ -3,14 +3,14 @@
 var fs = require("fs")
 var UglifyES = require("uglify-es")
 
-module.exports = function(input, output, options) {
-	function minify(input, output) {
-		var original = fs.readFileSync(input, "utf8"),
+module.exports = function(inputPath, outputPath, options) {
+	function minify(inputPath, outputPath) {
+		var original = fs.readFileSync(inputPath, "utf8"),
 			uglified = UglifyES.minify(original),
 			compressed = uglified.code
 
 		if (compressed) {
-			fs.writeFileSync(output, compressed, "utf8")
+			fs.writeFileSync(outputPath, compressed, "utf8")
 			return {original: original, compressed: compressed}
 		}
 		else if (uglified.error) console.log(uglified.error)
@@ -18,10 +18,10 @@ module.exports = function(input, output, options) {
 
 	function run() {
 		console.log("minifying...")
-		return minify(input, output)
+		return minify(inputPath, outputPath)
 	}
 
-	if (options && options.watch) fs.watchFile(input, run)
+	if (options && options.watch) fs.watchFile(inputPath, run)
 
 	return run() || null
 }
