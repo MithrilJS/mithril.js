cc578860e6340cbb811313bd29d0f1ae4b931f78
Isiah Meadows <contact@isiahmeadows.com>
2018-12-04 12:55:34 -0500

    Implement support for variadic arguments to `m.fragment`
    While I was at it, I refactored the common logic out of `hyperscript`.

---
+++ render/hyperscriptVnode.js
@@ -0,0 +1,25 @@
+"use strict"
+
+var Vnode = require("../render/vnode")
+
+// Call via `hyperscriptVnode.apply(startOffset, arguments)`
+module.exports = function() {
+	var attrs = arguments[this], start = this + 1, children
+
+	if (attrs == null) {
+		attrs = {}
+	} else if (typeof attrs !== "object" || attrs.tag != null || Array.isArray(attrs)) {
+		attrs = {}
+		start = this
+	}
+
+	if (arguments.length === start + 1) {
+		children = arguments[start]
+		if (!Array.isArray(children)) children = [children]
+	} else {
+		children = []
+		while (start < arguments.length) children.push(arguments[start++])
+	}
+
+	return Vnode("", attrs.key, attrs, children)
+}
