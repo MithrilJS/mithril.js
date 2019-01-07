39c20bdb34bd7d8ed225b0df75d2cd5021b4c9fd
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2019-01-07 04:56:09 -0500

    Handle `[` fragment selector. Fixes #2349 (#2352)
    * Handle `[` fragment selector. Fix #2349
    
    * Update tests
    
    * Simplify hyperscript function

--- render/hyperscript.js
+++ render/hyperscript.js
@@ -90,11 +90,12 @@ function hyperscript(selector) {
 	var vnode = hyperscriptVnode.apply(1, arguments)
 
 	if (typeof selector === "string") {
-		return execSelector(selectorCache[selector] || compileSelector(selector), vnode)
-	} else {
-		vnode.tag = selector
-		return vnode
+		vnode.children = Vnode.normalizeChildren(vnode.children)
+		if (selector !== "[") return execSelector(selectorCache[selector] || compileSelector(selector), vnode)
 	}
+	
+	vnode.tag = selector
+	return vnode
 }
 
 module.exports = hyperscript
