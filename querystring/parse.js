4ac6c29b4bf0679ccf24a67c8cbbff63bf5cd0c9
Hunter Dolan <hunterhdolan@gmail.com>
2019-08-29 08:55:00 -0400

    Finalize querystring fix and ensure working tests. Implements changes made in e58e918

--- querystring/parse.js
+++ querystring/parse.js
@@ -4,7 +4,7 @@ module.exports = function(string) {
 	if (string === "" || string == null) return {}
 	if (string.charAt(0) === "?") string = string.slice(1)
 
-	var entries = string.split("&"), data = {}, counters = {}
+	var entries = string.split("&"), counters = {}, data = {}
 	for (var i = 0; i < entries.length; i++) {
 		var entry = entries[i].split("=")
 		var key = decodeURIComponent(entry[0])
@@ -19,23 +19,24 @@ module.exports = function(string) {
 		for (var j = 0; j < levels.length; j++) {
 			var level = levels[j], nextLevel = levels[j + 1]
 			var isNumber = nextLevel == "" || !isNaN(parseInt(nextLevel, 10))
-			var isValue = j === levels.length - 1
 			if (level === "") {
 				var key = levels.slice(0, j).join()
-				if (counters[key] == null) counters[key] = 0
+				if (counters[key] == null) {
+					counters[key] = Array.isArray(cursor) ? cursor.length : 0
+				}
 				level = counters[key]++
 			}
 			// Disallow direct prototype pollution
 			else if (level === "__proto__") break
-			if (isValue) cursor[level] = value
+			if (j === levels.length - 1) cursor[level] = value
 			else {
 				// Read own properties exclusively to disallow indirect
 				// prototype pollution
-				value = Object.getOwnPropertyDescriptor(cursor, level)
-				if (value != null) value = value.value
-				if (value == null) value = cursor[level] = isNumber ? [] : {}
+				var desc = Object.getOwnPropertyDescriptor(cursor, level)
+				if (desc != null) desc = desc.value
+				if (desc == null) cursor[level] = desc = isNumber ? [] : {}
+				cursor = desc
 			}
-			cursor = value
 		}
 	}
 	return data
