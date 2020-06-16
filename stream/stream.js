ec92b85d9ac1090b1f0c250e625277aca584be5a
Isiah Meadows <contact@isiahmeadows.com>
2020-06-16 08:24:04 -0700

    Fix #2601

--- stream/stream.js
+++ stream/stream.js
@@ -29,7 +29,11 @@ function Stream(value) {
 			if (open(stream)) {
 				stream._changing()
 				stream._state = "active"
-				dependentStreams.forEach(function(s, i) { s(dependentFns[i](value)) })
+				// Cloning the list to ensure it's still iterated in intended
+				// order
+				dependentStreams.slice().forEach(function(s, i) {
+					if (open(s)) s(this[i](value))
+				}, dependentFns.slice())
 			}
 		}
 
