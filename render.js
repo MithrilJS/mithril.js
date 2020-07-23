af6e0fd2b8733268409366de9d27acc81e25348f
Oliver Russell <ojhrussell@gmail.com>
2020-07-23 08:49:26 +0100

    set window, document, requestAnimationFrame when missing
    When importing mithril as a module in node, it blows up because there
    is no window|document|requestAnimationFrame. This is particularly
    problematic for ECMAScript modules (as opposed to CommmonJS Modules)
    as there is no simple way to run code *before* importing a module. (There
    are a number of hacks using the dynamic `import()` function, but these
    aren't any fun.
    
    This commit makes it such that when mithril appears to be imported from
    inside node (by checking if `global.process.version` exists), it will
    set the three variables to `undefined` if they don't exist.

--- render.js
+++ render.js
@@ -1,3 +1,15 @@
 "use strict"
 
+// When running in node, we get reference errors on import
+// unless we have window, document, requestAnimationFrame
+try {
+    var isRunningInNode = global.process.version  // a good enough proxy
+    if (isRunningInNode){
+        global.window = global.window || undefined
+        global.document = global.document || undefined
+        global.requestAnimationFrame = global.requestAnimationFrame || undefined
+    }
+}
+finally {}
+
 module.exports = require("./render/render")(window)
