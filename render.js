d37564392f0420d989119b59fc80731182583085
Oliver Russell <ojhrussell@gmail.com>
2020-07-23 09:08:30 +0100

    fixup! set window, document, requestAnimationFrame when missing

--- render.js
+++ render.js
@@ -3,13 +3,15 @@
 // When running in node, we get reference errors on import
 // unless we have window, document, requestAnimationFrame
 try {
-    var isRunningInNode = global.process.version  // a good enough proxy
-    if (isRunningInNode){
-        global.window = global.window || undefined
-        global.document = global.document || undefined
-        global.requestAnimationFrame = global.requestAnimationFrame || undefined
-    }
+	var isRunningInNode = global.process.version // a good enough proxy
+	if (isRunningInNode){
+		global.window = global.window || undefined
+		global.document = global.document || undefined
+		global.requestAnimationFrame = global.requestAnimationFrame || undefined
+	}
 }
-finally {}
+finally {
+	null
+}
 
 module.exports = require("./render/render")(window)
