d390f883e2d2959ac0aa1c3c2df51d831c8f3302
Pierre-Yves <py@py.gy>
2022-06-09 13:50:27 +0200

    implement m.domFor() and use it internally to move and remove nodes. Fix #2780

---
+++ render/dom-for.js
@@ -0,0 +1,24 @@
+"use strict"
+
+var delayedRemoval = module.exports.delayedRemoval = new WeakMap
+
+module.exports.domFor = function *domFor(vnode, {generation} = {generation: undefined}) {
+	let {dom, domSize} = vnode
+	if (dom != null) {
+		if (domSize == null) {
+			if (delayedRemoval.get(dom) === generation) {
+				yield dom
+			}
+		} else {
+			let i = 0, next
+			while (i < domSize) {
+				next = dom.nextSibling
+				if (delayedRemoval.get(dom) === generation) {
+					yield dom
+					i++
+				}
+				dom = next
+			}
+		}
+	}
+}
