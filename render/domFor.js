a450bb743d3cf3b0e0ecd71ed34c42b42fbb62f2
Barney <barney.carroll@gmail.com>
2022-06-12 10:47:57 +0100

    Simplify domFor logic

--- render/domFor.js
+++ render/domFor.js
@@ -3,23 +3,16 @@
 var delayedRemoval = new WeakMap
 module.exports.delayedRemoval = delayedRemoval
 
-module.exports.domFor = function *domFor(vnode, {generation} = {generation: undefined}) {
-	let {dom, domSize} = vnode
-	if (dom != null) {
-		if (domSize == null) {
-			if (delayedRemoval.get(dom) === generation) {
-				yield dom
-			}
-		} else {
-			let i = 0, next
-			while (i < domSize) {
-				next = dom.nextSibling
-				if (delayedRemoval.get(dom) === generation) {
-					yield dom
-					i++
-				}
-				dom = next
-			}
+module.exports.domFor = function *domFor({dom, domSize}, {generation} = {}) {
+	if (dom != null) do {
+		const {nextSibling} = dom
+		
+		if (delayedRemoval.get(dom) === generation) {
+			yield dom
+			domSize--
 		}
+		
+		dom = nextSibling
 	}
+	while (domSize)
 }
