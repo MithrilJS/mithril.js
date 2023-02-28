bf3f1e9f359fc3407ec9a0ca48bc65a1d4019894
Pierre Grimaud <pieroxy@pieroxy.net>
2023-02-28 23:09:27 +0100

    ErrorManager allow for clients to register events (log and fail)

--- util/errorManager.js
+++ util/errorManager.js
@@ -8,35 +8,76 @@
 
 module.exports = {
   em: {
+    logEventListeners:[],
+    failEventListeners:[],
+
     log(level, message) {
-      switch (level) {
-        case 0:
-          console.error(message)
-          break;
-        case 1:
-          console.warn(message)
-          break;
-        case 2:
-          console.info(message)
-          break;
-        case 3:
-          console.debug(message)
-          break;
-      }
+      this.executeListeners(
+        this.logEventListeners, 
+        {level:level,message:message},
+        () => {
+          switch (level) {
+            case 0:
+              console.error(message)
+              break;
+            case 1:
+              console.warn(message)
+              break;
+            case 2:
+              console.info(message)
+              break;
+            case 3:
+              console.debug(message)
+              break;
+          }
+        })
     },
 
     fail(error, vnode) {
-      this.debugVnode(0, vnode)
-      if (e instanceof Error) 
-        throw error
-      else
-        throw new Error(e)
+      this.executeListeners(
+        this.failEventListeners,
+        {error:error, vnode:vnode},
+        () => {
+          this.debugVnode(0, vnode)
+          if (error instanceof Error) 
+            throw error
+          else
+            throw new Error(error)
+        }
+      )
     },
 
     debugVnode(level, vnode) {
       if (vnode) {
         this.log(level, "Context: " + JSON.stringify(vnode))
       }
+    },
+
+    addEventListener(name, callback) {
+      switch (name) {
+        case "log":
+          this.addToArray(this.logEventListeners, callback)
+          break
+        case "fail":
+          this.addToArray(this.failEventListeners, callback)
+          break
+      }
+    },
+
+    executeListeners(listeners, param, defaultHandler) {
+      let stopProcessing = false;
+      for (let i=0 ; i<listeners.length ; i++) {
+        listeners[i](param, () => {stopProcessing=true})
+        if (stopProcessing) return
+      }
+      defaultHandler()
+    },
+
+    addToArray(array, object) {
+      for (let i=0 ; i<array.length ; i++) {
+        if (array[i] === object) return
+      }
+      array.push(object)
     }
   }
 }
