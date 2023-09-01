c35a711831c5d79d6e9fe0fc875465a1eecbd46e
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-09-01 21:26:31 +0530

    Update request.js

--- request.js
+++ request.js
@@ -1,5 +1,5 @@
 "use strict"
 
-var mountRedraw = require("./mount-redraw")
+let mountRedraw = require("./mount-redraw")
 
 module.exports = require("./request/request")(typeof window !== "undefined" ? window : null, mountRedraw.redraw)
