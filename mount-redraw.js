bfdbc73186270b05a52c28d547ebb27887117b8c
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-09-01 21:27:24 +0530

    Update mount-redraw.js

--- mount-redraw.js
+++ mount-redraw.js
@@ -1,5 +1,5 @@
 "use strict"
 
-var render = require("./render")
+let render = require("./render")
 
 module.exports = require("./api/mount-redraw")(render, typeof requestAnimationFrame !== "undefined" ? requestAnimationFrame : null, typeof console !== "undefined" ? console : null)
