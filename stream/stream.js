56b8f62a7bb789bc52cfbe6d511c38f2c38051c1
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-09-01 21:38:19 +0530

    Update stream.js

--- stream/stream.js
+++ stream/stream.js
@@ -10,7 +10,7 @@ Stream.combine = combine
 Stream.scanMerge = scanMerge
 Stream["fantasy-land/of"] = Stream
 
-var warnedHalt = false
+let warnedHalt = false
 Object.defineProperty(Stream, "HALT", {
 	get: function() {
 		warnedHalt || console.log("HALT is deprecated and has been renamed to SKIP");
@@ -20,8 +20,8 @@ Object.defineProperty(Stream, "HALT", {
 })
 
 function Stream(value) {
-	var dependentStreams = []
-	var dependentFns = []
+	let dependentStreams = []
+	let dependentFns = []
 
 	function stream(v) {
 		if (arguments.length && v !== Stream.SKIP) {
@@ -52,7 +52,7 @@ function Stream(value) {
 	}
 
 	stream._map = function(fn, ignoreInitial) {
-		var target = ignoreInitial ? Stream() : Stream(fn(value))
+		let target = ignoreInitial ? Stream() : Stream(fn(value))
 		target._parents.push(stream)
 		dependentStreams.push(target)
 		dependentFns.push(fn)
@@ -63,7 +63,7 @@ function Stream(value) {
 		return stream._map(fn, stream._state !== "active")
 	}
 
-	var end
+	let end
 	function createEnd() {
 		end = Stream()
 		end.map(function(value) {
@@ -83,7 +83,7 @@ function Stream(value) {
 	stream["fantasy-land/ap"] = function(x) { return combine(function(s1, s2) { return s1()(s2()) }, [x, stream]) }
 
 	stream._unregisterChild = function(child) {
-		var childIndex = dependentStreams.indexOf(child)
+		let childIndex = dependentStreams.indexOf(child)
 		if (childIndex !== -1) {
 			dependentStreams.splice(childIndex, 1)
 			dependentFns.splice(childIndex, 1)
@@ -98,18 +98,18 @@ function Stream(value) {
 }
 
 function combine(fn, streams) {
-	var ready = streams.every(function(s) {
+	let ready = streams.every(function(s) {
 		if (s.constructor !== Stream)
 			throw new Error("Ensure that each item passed to stream.combine/stream.merge/lift is a stream.")
 		return s._state === "active"
 	})
-	var stream = ready
+	let stream = ready
 		? Stream(fn.apply(null, streams.concat([streams])))
 		: Stream()
 
-	var changed = []
+	let changed = []
 
-	var mappers = streams.map(function(s) {
+	let mappers = streams.map(function(s) {
 		return s._map(function(value) {
 			changed.push(s)
 			if (ready || streams.every(function(s) { return s._state !== "pending" })) {
@@ -121,7 +121,7 @@ function combine(fn, streams) {
 		}, true)
 	})
 
-	var endStream = stream.end.map(function(value) {
+	let endStream = stream.end.map(function(value) {
 		if (value === true) {
 			mappers.forEach(function(mapper) { mapper.end(true) })
 			endStream.end(true)
@@ -137,8 +137,8 @@ function merge(streams) {
 }
 
 function scan(fn, acc, origin) {
-	var stream = origin.map(function(v) {
-		var next = fn(acc, v)
+	let stream = origin.map(function(v) {
+		let next = fn(acc, v)
 		if (next !== Stream.SKIP) acc = next
 		return next
 	})
@@ -147,10 +147,10 @@ function scan(fn, acc, origin) {
 }
 
 function scanMerge(tuples, seed) {
-	var streams = tuples.map(function(tuple) { return tuple[0] })
+	let streams = tuples.map(function(tuple) { return tuple[0] })
 
-	var stream = combine(function() {
-		var changed = arguments[arguments.length - 1]
+	let stream = combine(function() {
+		let changed = arguments[arguments.length - 1]
 		streams.forEach(function(stream, i) {
 			if (changed.indexOf(stream) > -1)
 				seed = tuples[i][1](seed, stream())
@@ -165,8 +165,8 @@ function scanMerge(tuples, seed) {
 }
 
 function lift() {
-	var fn = arguments[0]
-	var streams = Array.prototype.slice.call(arguments, 1)
+	let fn = arguments[0]
+	let streams = Array.prototype.slice.call(arguments, 1)
 	return merge(streams).map(function(streams) {
 		return fn.apply(undefined, streams)
 	})
