b5857a7cd0567db00a5f852092b4958f52468928
kfule <kta@live.jp>
2024-10-01 07:04:42 +0900

    m.domFor(): workaround for unintentional mangling. Fix #2842 (#2981)

--- render/domFor.js
+++ render/domFor.js
@@ -2,9 +2,14 @@
 
 var delayedRemoval = new WeakMap
 
-function *domFor({dom, domSize}, {generation} = {}) {
+function *domFor(vnode, object = {}) {
+	// To avoid unintended mangling of the internal bundler,
+	// parameter destructuring is not used here.
+	var dom = vnode.dom
+	var domSize = vnode.domSize
+	var generation = object.generation
 	if (dom != null) do {
-		const {nextSibling} = dom
+		var nextSibling = dom.nextSibling
 
 		if (delayedRemoval.get(dom) === generation) {
 			yield dom
