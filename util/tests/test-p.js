eb0031cef5e3eaadb67a02de369520adf8258d55
Claudia Meadows <contact@claudiameadows.dev>
2024-10-03 03:25:01 -0700

    Normalize the callback style in `test-p.js`.

--- util/tests/test-p.js
+++ util/tests/test-p.js
@@ -3,89 +3,89 @@
 var o = require("ospec")
 var p = require("../p")
 
-o.spec("p", function() {
+o.spec("p", () => {
 	function test(prefix) {
-		o("returns path if no params", function () {
+		o("returns path if no params", () => {
 			var string = p(prefix + "/route/foo", undefined)
 
 			o(string).equals(`${prefix}/route/foo`)
 		})
-		o("skips interpolation if no params", function () {
+		o("skips interpolation if no params", () => {
 			var string = p(prefix + "/route/:id", undefined)
 
 			o(string).equals(`${prefix}/route/:id`)
 		})
-		o("appends query strings", function () {
+		o("appends query strings", () => {
 			var string = p(prefix + "/route/foo", {a: "b", c: 1})
 
 			o(string).equals(`${prefix}/route/foo?a=b&c=1`)
 		})
-		o("inserts template parameters at end", function () {
+		o("inserts template parameters at end", () => {
 			var string = p(prefix + "/route/:id", {id: "1"})
 
 			o(string).equals(`${prefix}/route/1`)
 		})
-		o("inserts template parameters at beginning", function () {
+		o("inserts template parameters at beginning", () => {
 			var string = p(prefix + "/:id/foo", {id: "1"})
 
 			o(string).equals(`${prefix}/1/foo`)
 		})
-		o("inserts template parameters at middle", function () {
+		o("inserts template parameters at middle", () => {
 			var string = p(prefix + "/route/:id/foo", {id: "1"})
 
 			o(string).equals(`${prefix}/route/1/foo`)
 		})
-		o("inserts variadic paths", function () {
+		o("inserts variadic paths", () => {
 			var string = p(prefix + "/route/:foo...", {foo: "id/1"})
 
 			o(string).equals(`${prefix}/route/id/1`)
 		})
-		o("inserts variadic paths with initial slashes", function () {
+		o("inserts variadic paths with initial slashes", () => {
 			var string = p(prefix + "/route/:foo...", {foo: "/id/1"})
 
 			o(string).equals(`${prefix}/route//id/1`)
 		})
-		o("skips template parameters at end if param missing", function () {
+		o("skips template parameters at end if param missing", () => {
 			var string = p(prefix + "/route/:id", {param: 1})
 
 			o(string).equals(`${prefix}/route/:id?param=1`)
 		})
-		o("skips template parameters at beginning if param missing", function () {
+		o("skips template parameters at beginning if param missing", () => {
 			var string = p(prefix + "/:id/foo", {param: 1})
 
 			o(string).equals(`${prefix}/:id/foo?param=1`)
 		})
-		o("skips template parameters at middle if param missing", function () {
+		o("skips template parameters at middle if param missing", () => {
 			var string = p(prefix + "/route/:id/foo", {param: 1})
 
 			o(string).equals(`${prefix}/route/:id/foo?param=1`)
 		})
-		o("skips variadic template parameters if param missing", function () {
+		o("skips variadic template parameters if param missing", () => {
 			var string = p(prefix + "/route/:foo...", {param: "/id/1"})
 
 			o(string).equals(`${prefix}/route/:foo...?param=%2Fid%2F1`)
 		})
-		o("handles escaped values", function() {
+		o("handles escaped values", () => {
 			var data = p(prefix + "/route/:foo", {"foo": ";:@&=+$,/?%#"})
 
 			o(data).equals(`${prefix}/route/%3B%3A%40%26%3D%2B%24%2C%2F%3F%25%23`)
 		})
-		o("handles unicode", function() {
+		o("handles unicode", () => {
 			var data = p(prefix + "/route/:ö", {"ö": "ö"})
 
 			o(data).equals(`${prefix}/route/%C3%B6`)
 		})
-		o("handles zero", function() {
+		o("handles zero", () => {
 			var string = p(prefix + "/route/:a", {a: 0})
 
 			o(string).equals(`${prefix}/route/0`)
 		})
-		o("handles false", function() {
+		o("handles false", () => {
 			var string = p(prefix + "/route/:a", {a: false})
 
 			o(string).equals(`${prefix}/route/false`)
 		})
-		o("handles dashes", function() {
+		o("handles dashes", () => {
 			var string = p(prefix + "/:lang-:region/route", {
 				lang: "en",
 				region: "US"
@@ -93,7 +93,7 @@ o.spec("p", function() {
 
 			o(string).equals(`${prefix}/en-US/route`)
 		})
-		o("handles dots", function() {
+		o("handles dots", () => {
 			var string = p(prefix + "/:file.:ext/view", {
 				file: "image",
 				ext: "png"
@@ -101,17 +101,17 @@ o.spec("p", function() {
 
 			o(string).equals(`${prefix}/image.png/view`)
 		})
-		o("merges query strings", function() {
+		o("merges query strings", () => {
 			var string = p(prefix + "/item?a=1&b=2", {c: 3})
 
 			o(string).equals(`${prefix}/item?a=1&b=2&c=3`)
 		})
-		o("merges query strings with other parameters", function() {
+		o("merges query strings with other parameters", () => {
 			var string = p(prefix + "/item/:id?a=1&b=2", {id: "foo", c: 3})
 
 			o(string).equals(`${prefix}/item/foo?a=1&b=2&c=3`)
 		})
-		o("consumes template parameters without modifying query string", function() {
+		o("consumes template parameters without modifying query string", () => {
 			var string = p(prefix + "/item/:id?a=1&b=2", {id: "foo"})
 
 			o(string).equals(`${prefix}/item/foo?a=1&b=2`)
@@ -202,8 +202,8 @@ o.spec("p", function() {
 			o(string).equals(prefix)
 		})
 	}
-	o.spec("absolute", function() { test("") })
-	o.spec("relative", function() { test("..") })
-	o.spec("absolute + domain", function() { test("https://example.com") })
-	o.spec("absolute + `file:`", function() { test("file://") })
+	o.spec("absolute", () => { test("") })
+	o.spec("relative", () => { test("..") })
+	o.spec("absolute + domain", () => { test("https://example.com") })
+	o.spec("absolute + `file:`", () => { test("file://") })
 })
