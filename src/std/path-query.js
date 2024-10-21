fee0cca857d4da6da6216310c9f523f3c9ea4847
Claudia Meadows <contact@claudiameadows.dev>
2024-10-20 22:48:42 -0700

    Boost `m.p` performance by a lot
    Turns out this was way more perf-sensitive than I initially thought. Also included a long comment to explain everything, as it's not immediately obvious just looking at it that it'd be perf-sensitive.

--- src/std/path-query.js
+++ src/std/path-query.js
@@ -1,3 +1,46 @@
+/*
+Caution: `m.p` and the failure path of `m.match` are both perf-sensitive. More so than you might
+think. And unfortunately, string indexing is incredibly slow.
+
+Suppose we're in a large CRUD app with 20 resources and 10 pages for each resource, for a total of
+200 routes. And further, suppose we're on a complicated management page (like a domain management
+page) with a grid of 50 rows and 8 routed icon links each. Each link has its URL constructed via
+`m.p(...)`, for a total of 400 calls. (This is high, but still realistic. At the time of writing,
+Namesilo's UI for selecting domains and performing batch operations on them is designed as a table
+with about that many icon links and up to 100 domains per page.)
+
+To meet 60 FPS, we generally have to have the whole page rendered in under 10ms for the browser to
+not skip frames. To give the user some buffer for view inefficiency, let's aim for 2ms of overhead
+for all the `m.match` and `m.p` calls. From some local benchmarking, the failure path of `m.match`
+requires about 1us/op, so 200 routes would come out to about 0.2ms. (The success path is well under
+0.1ms, so its overhead is negligible.) That leaves us about 1.8ms for 400 calls to `m.p(...)`. Do
+the math, and that comes out to a whopping 4.5 us/call for us to meet our deadline.
+
+I've tried the following for `m.p`, and most of them ended up being too slow. Times are for calls
+with two string interpolation parameters (the slow path), measured on an older laptop. The laptop
+experiences a roughly 30-60% perf boost when charging over when running from battery. The lower end
+is while charging, the higher end is while on battery.
+
+- A direct port of v2's `m.buildPathname`: 15-25 us
+	- This provides headroom for up to about 70 calls per frame.
+- Replace its inner `template.replace` with a `re.exec(template)` loop: 12-18 microseconds
+	- This provides headroom for up to about 100 calls per frame.
+- Switch from using match strings to computing positions from `exec.index`: 6.5-12 microseconds
+	- This provides headroom for up to about 150 calls per frame.
+- Iterate string directly: 2-3.5 microseconds
+	- This provides headroom for up to about 500 calls per frame.
+
+I've tried optimizing it further, but I'm running into the limits of string performance at this
+point. And the computing positions from `exec.index` is about the fastest I could get any
+regexp-based solution to go.
+
+Also, I tried at first restricting parameters to JS identifiers (like `m.match` parameters are, as
+I use named groups to generate the properties), but that, just on the regexp side, cut performance
+in more than half. The `exec.match` form, the ideal one for regexp-based solutions, slowed down
+from 12 microseconds to about 35-40 microseconds. And that would reduce headroom down to only about
+45-50 calls per frame. This rate is simply too slow to even be viable for some smaller apps.
+*/
+
 // Allowed terminators for `m.match`:
 // - `.` for `:file.:ext`
 // - `-` for `:lang-:locale`
@@ -6,133 +49,198 @@
 // Escape with `\\`
 // Use `*rest` for rest
 
-// Caution: `m.p` and the failure path of `m.match` are both perf-sensitive. It only takes a couple
-// hundred `m.p` calls with parameters to amount to about 1ms, and there's only about 10ms total
-// that one can reasonably use to render stuff. And it's reasonable to expect several dozen `m.p`
-// calls with parameters even in a medium-sized app.
-//
-// The more complicated one, `m.match`, fortunately is fairly cheap in the common case of mismatch.
-// However, `m.p`'s common case is *with variables*, and getting the runtime of that down wasn't
-// easy. (`m.p` for context was designed with usage like `m(m.Link, {href: m.p(...)})` in mind.)
-
 import {hasOwn} from "../util.js"
 
 var toString = {}.toString
 
-var invalidTemplateChars = /[:*][$_\p{IDS}](?![$\p{IDC}]*(?![:*]))/u
-var invalidMatchTemplate = /\/\/|[:*](?![$_\p{IDS}][$\p{IDC}]*(?![:*]))|\*.*?[^$\p{IDC}]|:([$_\p{IDS}][$\p{IDC}]*)[^$\p{IDC}].*?[:*]\1(?![$\p{IDC}])/u
-var escapeOrParameter = /\\.|[:*][$_\p{IDS}][$\p{IDC}]*/ug
-var escapeOnly = /\\(.)/g
+var invalidMatchTemplate = /\/\/|[:*][^$_\p{IDS}]|[:*].[$\p{IDC}]*[:*]|\*.*?[^$\p{IDC}]|:([$_\p{IDS}][$\p{IDC}]*)[^$\p{IDC}].*?[:*]\1(?![$\p{IDC}])/u
 // I escape literal text so people can use things like `:file.:ext` or `:lang-:locale` in routes.
 // This is all merged into one pass so I don't also accidentally escape `-` and make it harder to
 // detect it to ban it from template parameters.
 var matcherCompile = /([:*])([$_\p{IDS}][$\p{IDC}]*)|\\\\|\\?([$^*+.()|[\]{}])|\\(.)/ug
 
-var serializeQueryValue = (qs, prefix, value) => {
-	if (value == null || value === false) return
-	if (Array.isArray(value)) {
-		for (var i of value) {
-			serializeQueryValue(qs, `${prefix}[]`, i)
-		}
-	} else {
-		if (typeof value === "object") {
-			var proto = Object.getPrototypeOf(value)
-			if (proto == null || proto === Object.prototype || toString.call(value) === "[object Object]") {
-				for (var k in value) {
-					if (hasOwn.call(value, k)) {
-						serializeQueryValue(qs, `${prefix}[${k}]`, value[k])
-					}
-				}
-				return
+var serializeQueryValue = (pq, result, prefix, value) => {
+	var proto
+
+	if (value != null && value !== false) {
+		if (Array.isArray(value)) {
+			for (var i of value) {
+				result = serializeQueryValue(pq, result, `${prefix}[]`, i)
 			}
+		} else if (
+			typeof value === "object" &&
+			((proto = Object.getPrototypeOf(value)) == null || proto === Object.prototype || toString.call(value) === "[object Object]")
+		) {
+			for (var k in value) {
+				if (hasOwn.call(value, k)) {
+					result = serializeQueryValue(pq, result, `${prefix}[${k}]`, value[k])
+				}
+			}
+		} else {
+			var sep = pq.s
+			pq.s = "&"
+			result += sep + encodeURIComponent(prefix) + (value === true ? "" : `=${
+				typeof value === "number" || typeof value === "bigint"
+					? value
+					: encodeURIComponent(value)
+			}`)
 		}
-		qs.v += qs.s + encodeURIComponent(prefix) + (value === true ? "" : `=${encodeURIComponent(value)}`)
-		qs.s = "&"
 	}
+
+	return result
 }
 
-var makeQueryBuilder = (sep, value) => ({s: sep, v: value})
-
-var q = (params) => {
-	var qs = makeQueryBuilder("", "")
+var serializeQueryParams = (sep, value, exclude, params) => {
+	var pq = {s: sep}
 	for (var key in params) {
-		if (hasOwn.call(params, key)) serializeQueryValue(qs, key, params[key])
+		if (hasOwn.call(params, key) && !exclude.includes(key)) {
+			value = serializeQueryValue(pq, value, key, params[key])
+		}
 	}
-	return qs.v
+	return value
 }
+
+var q = (params) => serializeQueryParams("", "", [], params)
+
+var QUERY = 0
+var ESCAPE = 1
+var CHAR = 2
+// Structure:
+// Bit 0: is raw
+// Bit 1: is next
+// Bit 2: always set
+var VAR_START = 4
+// var RAW_VAR_START = 5
+var VAR_NEXT = 6
+// var RAW_VAR_NEXT = 7
+var STATE_IS_RAW = 1
+var STATE_IS_NEXT = 2
+
 
 // Returns `path` from `template` + `params`
+/**
+ * @param {string} template
+ * @param {undefined | null | Record<string, any>} params
+ */
 var p = (template, params) => {
-	if (invalidTemplateChars.test(template)) {
-		throw new SyntaxError("Template parameter names must be separated by either a '/', '-', or '.'.")
-	}
-	if (params == null) return template.replace(escapeOnly, "$1")
-	var queryIndex = template.indexOf("?")
-	var hashIndex = template.indexOf("#")
-	var queryEnd = hashIndex < 0 ? template.length : hashIndex
-	var pathEnd = queryIndex < 0 ? queryEnd : queryIndex
-	var path = template.slice(0, pathEnd)
-	var inTemplate = new Set()
-	var resolved = ""
-	var start = escapeOrParameter.lastIndex = 0
-	var exec
+	// This carefully only iterates the template once.
+	var prev = 0
+	var start = 0
+	var state = CHAR
+	// An array is fine. It's almost never large enough for the overhead of hashing to pay off.
+	var inTemplate = []
+	// Used for later.
+	var hash = ""
+	var queryIndex = -1
+	var hashIndex = -1
+	var result = ""
+	var sep = "?"
 
-	while ((exec = escapeOrParameter.exec(path)) != null) {
-		var index = exec.index
-		resolved += path.slice(start, index)
-		start = escapeOrParameter.lastIndex
-		if (path[index] === "\\") {
-			start = index + 1
-		} else {
-			var key = path.slice(index + 1, start)
-			inTemplate.add(key)
-			key = params[key]
-			resolved += (
-				key != null
-					// Escape normal parameters, but not variadic ones.
-					? (path[index] === "*" ? key : encodeURIComponent(`${key}`))
-					// If no such parameter exists, don't interpolate it.
-					: path.slice(index, start)
-			)
+	var NOT_VAR_NEXT = VAR_NEXT - 1
+
+	// Using `for ... of` so the engine can do bounds check elimination more easily.
+	for (var i = 0;; i++) {
+		var ch = template.charAt(i)
+
+		if (
+			state > NOT_VAR_NEXT &&
+			(ch === "" || ch === "#" || ch === "?" || ch === "\\" || ch === "/" || ch === "." || ch === "-")
+		) {
+			var segment = template.slice(start + 1, i)
+
+			// If no such parameter exists, don't interpolate it.
+			if (params != null && params[segment] != null) {
+				inTemplate.push(segment)
+				segment = `${params[segment]}`
+
+				// Escape normal parameters, but not variadic ones.
+				// eslint-disable-next-line no-bitwise
+				if (state & STATE_IS_RAW) {
+					var newHashIndex = segment.indexOf("#")
+					var newQueryIndex = (newHashIndex < 0 ? segment : segment.slice(0, newHashIndex)).indexOf("?")
+					if (newQueryIndex >= 0) {
+						sep = "&"
+						queryIndex = result.length + (prev - start) + newQueryIndex
+					}
+					if (newHashIndex >= 0) {
+						hashIndex = result.length + (prev - start) + newHashIndex
+					}
+				} else {
+					segment = encodeURIComponent(segment)
+				}
+
+				// Drop the preceding `:`/`*`/`\` character from the appended segment
+				if (prev !== start) {
+					result += template.slice(prev, start)
+				}
+
+				result += segment
+
+				// Start from the next end
+				prev = i
+			}
 		}
-	}
 
-	resolved += path.slice(start)
+		if (ch === "#") {
+			if (hashIndex < 0) hashIndex = i
+		} else if (ch !== "") {
+			if (state === QUERY) {
+				// do nothing
+			} else if (ch === "?") {
+				// The query start cannot be escaped. It's a proper URL delimiter.
+				if (queryIndex < 0) {
+					queryIndex = i
+					sep = "&"
+				} else {
+					// Inject an `&` in place of a `?`. Note that `sep === "&"`
+					if (prev !== i) result += template.slice(prev, i)
+					result += "&"
+					prev = i + 1
+				}
+				state = QUERY
+			} else if (state === ESCAPE) {
+				// Drop the preceding `\` character from the appended segment
+				if (prev !== start) {
+					result += template.slice(prev, start)
+				}
 
-	// In case the template substitution adds new query/hash parameters.
-	var newQueryIndex = resolved.indexOf("?")
-	var newHashIndex = resolved.indexOf("#")
-	var newQueryEnd = newHashIndex < 0 ? resolved.length : newHashIndex
-	var newPathEnd = newQueryIndex < 0 ? newQueryEnd : newQueryIndex
-	var qs = makeQueryBuilder("?", resolved.slice(0, newPathEnd))
+				state = CHAR
+				start = prev = i
+			} else if (ch === "\\") {
+				start = i
+				state = ESCAPE
+			} else if (ch === ":" || ch === "*") {
+				if (state > CHAR) {
+					throw new SyntaxError("Template parameter names must be separated by either a '/', '-', or '.'.")
+				}
+				// eslint-disable-next-line no-bitwise
+				state = VAR_START | (ch === "*")
+				start = i
+			} else if (ch === "/" || ch === "." || ch === "-") {
+				state = CHAR
+			} else if (state > CHAR) {
+				// eslint-disable-next-line no-bitwise
+				state |= STATE_IS_NEXT
+			}
 
-	if (queryIndex >= 0) {
-		qs.v += template.slice(queryIndex, queryEnd)
-		qs.s = "&"
-	}
-
-	if (newQueryIndex >= 0) {
-		qs.v += qs.s + resolved.slice(newQueryIndex, newQueryEnd)
-		qs.s = "&"
-	}
-
-	for (var key in params) {
-		if (hasOwn.call(params, key) && !inTemplate.has(key)) {
-			serializeQueryValue(qs, key, params[key])
+			continue
 		}
-	}
 
-	if (hashIndex >= 0) {
-		qs.v += template.slice(hashIndex)
-	} else {
-		qs.s = ""
-	}
+		if (prev === 0 && params == null) {
+			return template
+		}
 
-	if (newHashIndex >= 0) {
-		qs.v += qs.s + resolved.slice(newHashIndex)
-	}
+		if (prev < template.length) {
+			result += template.slice(prev)
+		}
 
-	return qs.v
+		if (hashIndex >= 0) {
+			hash = result.slice(hashIndex)
+			result = result.slice(0, hashIndex)
+		}
+
+		return serializeQueryParams(sep, result, inTemplate, params) + hash
+	}
 }
 
 /** @typedef {RegExp & {r: number, p: URLSearchParams}} Matcher */
