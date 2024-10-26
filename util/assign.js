5aad5566c966aea520fc3bf4ffa217160bcba0cb
kfule <kta@live.jp>
2024-10-27 00:28:07 +0900

    remove polyfill for Object.assign()
    The polyfill doesn't support multiple source objects, and almost all browsers now natively support Object.assign().

--- util/assign.js
+++ util/assign.js
@@ -1,10 +1,4 @@
 // This exists so I'm only saving it once.
 "use strict"
 
-var hasOwn = require("./hasOwn")
-
-module.exports = Object.assign || function(target, source) {
-	for (var key in source) {
-		if (hasOwn.call(source, key)) target[key] = source[key]
-	}
-}
+module.exports = Object.assign
