c521cbf7375bfae21e59cb9636fdcd2ba9cd0454
kfule <kta@live.jp>
2025-10-06 19:36:59 +0900

    normalizeChildren: perform key consistency checks after normalization for better performance
    This change merges multiple loops into a single one and reduces the number of comparisons needed for consistency checking, resulting in improved performance.
    Additionally, since key checks are now performed on normalized vnodes, the consistency checks become more accurate.
    All existing tests pass.
    
    The comments have also been clarified with reference to the original commit 6c562d2.

--- render/vnode.js
+++ render/vnode.js
@@ -15,23 +15,20 @@ Vnode.normalizeChildren = function(input) {
 	// Normalize children in place for performance
 	// if vnodes are NOT component vnodes.
 	var children = input[isNormalizationDeferred] ? [] : input
-	if (input.length) {
-		var isKeyed = input[0] != null && input[0].key != null
-		// Note: this is a *very* perf-sensitive check.
-		// Fun fact: merging the loop like this is somehow faster than splitting
-		// it, noticeably so.
-		for (var i = 1; i < input.length; i++) {
-			if ((input[i] != null && input[i].key != null) !== isKeyed) {
-				throw new TypeError(
-					isKeyed && (input[i] == null || typeof input[i] === "boolean")
-						? "In fragments, vnodes must either all have keys or none have keys. You may wish to consider using an explicit keyed empty fragment, m.fragment({key: ...}), instead of a hole."
-						: "In fragments, vnodes must either all have keys or none have keys."
-				)
-			}
-		}
-		for (var i = 0; i < input.length; i++) {
-			children[i] = Vnode.normalize(input[i])
-		}
+	// Count the number of keyed normalized vnodes for consistency check.
+	// Note: this is a perf-sensitive check.
+	// Fun fact: merging the loop like this is somehow faster than splitting
+	// the check within updateNodes(), noticeably so.
+	var numKeyed = 0
+	for (var i = 0; i < input.length; i++) {
+		children[i] = Vnode.normalize(input[i])
+		if (children[i] != null && children[i].key != null) numKeyed++
+	}
+	if (numKeyed !== 0 && numKeyed !== input.length) {
+		throw new TypeError(children.includes(null)
+			? "In fragments, vnodes must either all have keys or none have keys. You may wish to consider using an explicit keyed empty fragment, m.fragment({key: ...}), instead of a hole."
+			: "In fragments, vnodes must either all have keys or none have keys."
+		)
 	}
 	return children
 }
